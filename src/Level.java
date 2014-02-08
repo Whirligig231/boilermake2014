73be229d1bfbb9d3c080176b4f216bc1c20aed7d
shanebruggeman <bruggess@rose-hulman.edu>
2014-02-08 02:03:05 -0500

    More improvements on the file reader

--- src/Level.java
+++ src/Level.java
@@ -1,31 +1,28 @@
+import java.awt.Dimension;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public class Level {
 
-	private int size;
+	private Dimension dimensions;
 	private int time;
 	private Point2D ballPosition;
 	
-	private ArrayList<Goal> goals;
-	private ArrayList<GameObject> onScreenObjects;
-	private ArrayList<GameObject> offScreenObjects;
-	
 	public Level(){
-		this.size = 0;
+		this.dimensions = new Dimension(0,0);
 		this.time = 0;
-		this.ballPosition = null;
-		this.goals = new ArrayList<Goal>();
-		this.onScreenObjects = new ArrayList<GameObject>();
-		this.offScreenObjects = new ArrayList<GameObject>();
 	}
 	
-	public void setSize(int size){
-		this.size = size;
+//	private ArrayList<Goal> goals;
+//	private ArrayList<GameObject> onScreenObjects;
+//	private ArrayList<GameObject> offScreenObjects;
+	
+	public void setDimensions(Dimension dimension){
+		this.dimensions = dimension;
 	}
 	
-	public int getSize(){
-		return this.size;
+	public Dimension getDimensions(){
+		return this.dimensions;
 	}
 	
 	public void setTime(int time){
@@ -40,17 +37,17 @@ public class Level {
 		this.ballPosition = ballPosition;
 	}
 	
-	public void addGoal(Goal nextGoal){
-		this.goals.add(nextGoal);
-	}
-	
-	public void addOnScreenObject(GameObject nextOnScreenObject){
-		this.onScreenObjects.add(nextOnScreenObject);
-	}
-	
-	public void addOffScreenObject(GameObject nextOffScreenObject){
-		this.offScreenObjects.add(nextOffScreenObject);
-	}
+//	public void addGoal(Goal nextGoal){
+//		this.goals.add(nextGoal);
+//	}
+//	
+//	public void addOnScreenObject(GameObject nextOnScreenObject){
+//		this.onScreenObjects.add(nextOnScreenObject);
+//	}
+//	
+//	public void addOffScreenObject(GameObject nextOffScreenObject){
+//		this.offScreenObjects.add(nextOffScreenObject);
+//	}
 	
 	
 	
--- src/LevelReader.java
+++ src/LevelReader.java
@@ -1,7 +1,9 @@
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.logging.Level;
 
 
 public class LevelReader {
@@ -16,7 +18,8 @@ public class LevelReader {
 		
 	public LevelReader(File levelFile){
 		this.text = levelFile;
-//		this.level = new Level();
+		this.level = new Level();
+		
 		try {
 			Scanner scanner = new Scanner(text);
 			StringBuilder fileText = new StringBuilder();
@@ -30,10 +33,6 @@ public class LevelReader {
 		}
 	}
 	
-	/**
-	 * Constructs the Level object and initializes all its variables
-	 * @param fileText the text file, broken up by <
-	 */
 	private void setup(String[] fileText){
 		
 		String sizeString = fileText[SIZE_STRING_POSITION];
@@ -41,10 +40,53 @@ public class LevelReader {
 		String timeString = fileText[TIME_STRING_POSITION];
 		String startString = fileText[START_STRING_POSITION];
 		
-		
+		processSize(sizeString);
+		processGoals(goalsString);
+		processTime(timeString);
+		processStart(startString);
 	}
 	
+	private void processSize(String sizeString){
+		
+		int startIndex = sizeString.indexOf(" ") + 1;
+		int endIndex = sizeString.indexOf(">") - 1;
+		
+		//separates into x and y
+		String[] pos = sizeString.substring(startIndex,endIndex).split(",");
+		
+		int sizeX = Integer.parseInt(pos[0]);
+		int sizeY = Integer.parseInt(pos[1]);
+		
+		this.level.setDimensions(new Dimension(sizeX,sizeY));
+	}
 	
+	private void processGoals(String goalsString){
+		//
+	}
+	
+	private void processTime(String timeString){
+		
+		int startIndex = timeString.indexOf(" ") + 1;
+		int endIndex = timeString.indexOf(">");
+		
+		int time = Integer.parseInt(timeString.substring(startIndex,endIndex));
+		
+		this.level.setTime(time);
+	}
+	
+	private void processStart(String startString){
+		
+		int startIndex = startString.indexOf("[") + 1;
+		int endIndex = startString.indexOf("]");
+		
+		//separates into x and y
+		String[] pos = startString.substring(startIndex,endIndex).split(",");
+		
+		int startX = Integer.parseInt(pos[0]);
+		int startY = Integer.parseInt(pos[1]);
+		
+		this.level.setBallPosition(new Point2D.Double(startX,startY));
+	}
 	
 	
 	
