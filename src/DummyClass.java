b9d44fa9230b4f329ab6f365845f604bc2153450
Whirligig231 <gigilrihw@gmail.com>
2014-02-07 22:04:40 -0500

    Testing the Eclipse project.

---
+++ src/DummyClass.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author schulzcc.
+ *         Created Feb 7, 2014.
+ */
+public class DummyClass {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+		JOptionPane.showMessageDialog(null,"DOOLAN IS GAY");
+	}
+
+}
