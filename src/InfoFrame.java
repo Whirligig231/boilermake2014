29765eb06d8b12d3af61ceca68d49819b5c24107
xxshadowfang <xxshadowfang@gmail.com>
2014-02-09 06:00:36 -0500

    instructions png added and a new level

--- src/InfoFrame.java
+++ src/InfoFrame.java
@@ -2,18 +2,22 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 
 public class InfoFrame extends JFrame {
 
 public InfoFrame(){
 	super();
+	this.setSize(850,1000);
 	JPanel instrPanel = new JPanel();
-	ImageIcon icon = new ImageIcon(" "); 
+	ImageIcon icon = new ImageIcon("./Graphics/instructions.png/"); 
 	JLabel label = new JLabel(); 
 	label.setIcon(icon); 
 	instrPanel.add(label); 
-	this.add(instrPanel);
+	JScrollPane spane = new JScrollPane(instrPanel);
+	this.add(spane);
+	this.setVisible(true);
 	
 }
 }
